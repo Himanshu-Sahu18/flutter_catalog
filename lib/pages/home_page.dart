76c6ba96797bb120d7189f164dff739aadf92db0
Himanshu-Sahu18 <sahuh3707@gmail.com>
2023-08-31 17:55:10 +0530

    day7 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productsData).map((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +40,36 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatalogModel.items[index]);
-          },
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 15, crossAxisSpacing: 15),
+            itemBuilder: (context, index) {
+              final item = CatalogModel.items[index];
+              return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  child: GridTile(
+                    header: Container(
+                      decoration: const BoxDecoration(color: Colors.deepPurple),
+                      child: Text(
+                        item.name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    footer: Container(
+                      decoration: const BoxDecoration(color: Colors.black),
+                      child: Text(
+                        item.price.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    child: Image.network(item.image),
+                  ));
+            },
+            itemCount: CatalogModel.items.length,
+          )),
       drawer: const MyDrawer(),
     );
   }
